e4a66172e9339b3238ce824efb863d25cb574e15
Fatima <fatimabret@outlook.com>
2024-10-13 18:30:41 -0300

    1ra parte de lote de datos cargada

--- SQLnetflix.sql
+++ SQLnetflix.sql
@@ -4,6 +4,7 @@ Bongiovanni, Iara Aylen
 Bret, Fatima Maria Luz
 Gonzalez, Ariel Rafael
 */
+
 create database netflix;
 use netflix;
 select * from netflix_titles order by show_id asc;
@@ -14,14 +15,14 @@ ALTER TABLE netflix_titles ALTER COLUMN show_id INT NOT NULL
 --Definición de datos
 --Tabla actor
 CREATE TABLE Actor(
-  id_actor INT NOT NULL,
+  id_actor INT identity(1,1),
   nomyape VARCHAR(100) NOT NULL,
   CONSTRAINT PK_id_actor PRIMARY KEY (id_actor)
 );
 
 --Tabla contenido
 CREATE TABLE Contenido(
-  id_contenido INT NOT NULL,
+  id_contenido INT identity(1,1),
   type VARCHAR(50) NOT NULL,
   rating VARCHAR(50) NOT NULL,
   date_added DATE NOT NULL,
@@ -30,14 +31,14 @@ CREATE TABLE Contenido(
 
 --Tabla country
 CREATE TABLE Country(
-  id_country INT NOT NULL,
+  id_country INT identity(1,1),
   nombre VARCHAR(100) NOT NULL,
   CONSTRAINT PK_id_country PRIMARY KEY (id_country)
 );
 
 --Tabla director
 CREATE TABLE Director(
-  id_director INT NOT NULL,
+  id_director INT identity(1,1),
   nomyape VARCHAR(100) NOT NULL,
   PRIMARY KEY (id_director)
 );
@@ -58,7 +59,7 @@ CREATE TABLE Show(
 
 --Tabla director_show
 CREATE TABLE Director_show(
-  id_show INT NOT NULL,
+  id_show INT identity(1,1),
   id_director INT NOT NULL,
   CONSTRAINT PK_id_show_id_director PRIMARY KEY (id_show, id_director),
   CONSTRAINT FK_id_show_director_show FOREIGN KEY (id_show) REFERENCES Show(id_show),
@@ -67,7 +68,7 @@ CREATE TABLE Director_show(
 
 --Tabla elenco
 CREATE TABLE Elenco(
-  id_show INT NOT NULL,
+  id_show INT identity(1,1),
   id_actor INT NOT NULL,
   CONSTRAINT PK_id_show_id_actor_elenco PRIMARY KEY (id_show, id_actor),
   CONSTRAINT FK_id_show_elenco FOREIGN KEY (id_show) REFERENCES Show(id_show),
@@ -76,7 +77,7 @@ CREATE TABLE Elenco(
 
 --Tabla genero
 CREATE TABLE Genero(
-  id_genero INT NOT NULL,
+  id_genero INT identity(1,1),
   descripcion VARCHAR(50) NOT NULL,
   id_show INT NOT NULL,
   CONSTRAINT PK_id_genero PRIMARY KEY (id_genero),
@@ -84,3 +85,40 @@ CREATE TABLE Genero(
 );
 
 --Carga de datos
+insert into country(nombre)
+values ((select country from netflix_titles WHERE show_id = 2));
+select * from country
+INSERT INTO Country (nombre)
+VALUES ('India'),
+	('United Kingdom'),
+	('France');
+
+INSERT INTO Director (nomyape)
+VALUES ('Kirsten Johnson'),
+		('Julien Leclercq'),
+		('Mike Flanagan'),
+		('Robert Cullen, José Luis Ucha'),
+		('Haile Gerima');
+select * from Director
+
+INSERT INTO Contenido (type, rating, date_added)
+VALUES ('Movie', 'PG-13', '2021-09-25'),
+		('TV Show', 'TV-MA', '2021-09-24'),
+		('Movie', 'PG', '2021-09-24'),
+		('Movie', 'TV-MA', '2021-09-24'),
+		('TV Show', 'TV-14', '2021-09-24');
+select * from Contenido
+
+INSERT INTO Actor (nomyape)
+VALUES ('Ama Qamata'),
+		('Khosi Ngema'),
+		('Gail Mabalane'),
+		('Thabang Molaba'),
+		('Dillon Windvogel'),
+		('Sami Bouajila'),
+		('Tracy Gotoas'),
+		('Samuel Jouy'),
+		('Nabiha Akkari'),
+		('Sofiane Zermani');
+select * from Actor
+
